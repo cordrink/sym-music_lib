a0b5ed433e81f897655edfe1c36f4d0b645a1d4c
cordrink <masodavictorjudicael@gmail.com>
2025-02-01 22:57:25 +0100

    style: Mise a jour de la barre de navigation

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -182,4 +182,9 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
         return $this;
     }
+
+    public function getFullName(): ?string
+    {
+        return $this->name.' '.$this->lastName;
+    }
 }
